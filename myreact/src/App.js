e014389b0ec896ef5e85046b758b0d31fee258be
starcman <44arcman@gmail.com>
2022-08-04 04:28:35 +0530

    added docker for testing & production

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -6,17 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>This is the our startup location</p>
+        <p>This is our way in. Let's go!!!</p>
+        <button>Attack</button>
       </header>
     </div>
   );
